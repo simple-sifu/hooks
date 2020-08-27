cd59b0d498f3e54f24903c063a826d0068ccb3f9
simple-sifu <kattyntommy@gmail.com>
2020-08-27 00:09:10 -0400

    add changes.

--- src/Board.js
+++ src/Board.js
@@ -2,12 +2,33 @@ import React from 'react'
 import Square from './Square'
 
 export default class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
+  }
     renderSquare(i) {
-      return <Square />;
+      return <Square 
+              value={this.state.squares[i]}
+              onClick={() => this.handleClick(i)}
+              
+              />;
     }
   
+    handleClick(i){
+      const squares = this.state.squares.slice();
+      squares[i] = this.state.xIsNext ? 'X':"O";
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+        
+      });
+    }
+
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player:' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
